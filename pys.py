5ab02973821bedd8befcffb03d81724bc06a3ada
angshu-min-js <angshu.gupta789@gmail.com>
2015-08-10 10:28:12 +0530

    commit

--- pys.py
+++
@@ -1,5 +0,0 @@
-from rockstar import RockStar
-
-python_code = "print('Welcome, to TensorFlow')"
-rock_it_bro = RockStar(days=400, file_name='__tensortest__.py', code=python_code)
-rock_it_bro.make_me_a_rockstar()
