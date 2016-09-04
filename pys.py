b6eae9aa0af58f40722434c0a811031f9e27c04d
codingang <angshu.gupta789@gmail.com>
2016-09-04 20:54:52 +0530

    update

---
+++ pys.py
@@ -0,0 +1,5 @@
+from rockstar import RockStar
+
+python_code = "print('Welcome, to TensorFlow')"
+rock_it_bro = RockStar(days=400, file_name='__tensortest__.py', code=python_code)
+rock_it_bro.make_me_a_rockstar()
